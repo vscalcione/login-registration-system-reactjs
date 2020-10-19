589f43b75aaf73051d0f0a250a31d1abfbc5cb8b
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-19 19:09:41 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 import { HeaderComponent } from "./components/header/HeaderComponent";
 import { FormComponent } from "./components/form/FormComponent";
 import { ProfileImageComponent } from "./components/profile-image/ProfileImageComponent";
 
-class App extends Component {
-  render() {
-    return (
-      <main id="main-area">
-        <section id="register">
-          <div className="row m-0">
-            <div className="col-lg-4 offset-lg-2">
-              <HeaderComponent />
-              <ProfileImageComponent />
-              <FormComponent />
-            </div>
+export const App = () => {
+  return (
+    <main id="main-area">
+      <section id="register">
+        <div className="row m-0">
+          <div className="col-lg-4 offset-lg-2">
+            <HeaderComponent />
+            <ProfileImageComponent />
+            <FormComponent />
           </div>
-        </section>
-      </main>
-    );
-  }
-}
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default App;
--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -2,37 +2,49 @@ import React from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export const HeaderComponent = () => {
+import { withRouter, useHistory, useLocation } from "react-router-dom";
+
+export const HeaderComponent = withRouter(({ history }) => {
   AOS.init();
+
+  const location = useLocation();
+  const currentUrl = location.pathname;
 
   return (
     <>
       <div
         className="text-center pb-5"
         data-aos="fade-right"
-        data-aos-delay="100"
+        data-aos-delay="200"
       >
         <h1
           className="login-title text-dark"
           style={{ fontFamily: "quicksand" }}
         >
-          Registration/Login
+          {currentUrl === "/" ? "Registration" : "Login"}
         </h1>
         <p className="font-ubuntu text-black-50" id="register-p">
-          Register and enjoy the additional features
+          {currentUrl === "/"
+            ? "Register and enjoy the additional features"
+            : ""}
         </p>
         <span className="font-ubuntu text-black-50">
-          Are you already registered?
+          {currentUrl === "/"
+            ? "Are you already registered?"
+            : "Are you already not registered?"}
           <br />
           <button
             type=""
             className="btn btn-warning rounded-pill text-dark px-4"
             style={{ fontFamily: "quicksand", marginTop: "22px" }}
+            onClick={() => {
+              history.push("/login");
+            }}
           >
-            Login
+            {currentUrl === "/" ? "Login" : "Register"}
           </button>
         </span>
       </div>
     </>
   );
-};
+});
